d85d0cfa82e3ccc88cff6bbe508bff700c4e8124
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-06 20:23:51 -0500

    added the files necessary for reflection 6

---
+++ Assignments/Intersector.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Intersector(inputArg1,inputArg2)
+%INTERSECTOR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
