4c983a9a8527f073c5553379471441edeb2040aa
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-12 20:48:56 -0500

    implemented the crossProduct method

--- Project/GrahamScan/src/JarvisImplementation.java
+++ Project/GrahamScan/src/JarvisImplementation.java
@@ -6,7 +6,17 @@ public class JarvisImplementation {
 		
 	}
 	
+	// computes the cross product of 2 vectors
 	public static int crossProduct(Point a, Point b, Point c) {
-		return 0;
+		int result= (b.getY()-a.getY())*(c.getX()-b.getX())-
+				(b.getX()-a.getX())*(c.getY()-b.getY());
+		
+		if (result==9) {
+			return 0; // Point is collinear and we take the furthest of these points
+		}
+		if(result>0) {
+			return 1; // point is furthest away
+		}
+		return -1; // a better match can be found
 	}
 }
