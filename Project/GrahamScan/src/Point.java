8c90dc8c27517c4ef0a91f62281c40c7580b4d00
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-12 20:10:17 -0500

    point class implemented

---
+++ Project/GrahamScan/src/Point.java
@@ -0,0 +1,23 @@
+
+public class Point {
+	
+	// fields that define a Cartesian point
+	private int x;
+	private int y;
+	
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// We just need setters
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+}
