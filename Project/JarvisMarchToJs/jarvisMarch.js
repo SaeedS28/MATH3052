0f99b5e4b8e60480e3c67fe5a07859d9f8db2d72
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-16 13:50:20 -0500

    created a point class.

--- Project/JarvisMarchToJs/jarvisMarch.js
+++ Project/JarvisMarchToJs/jarvisMarch.js
@@ -1,2 +1,14 @@
-console.log("It works!!!");
+/**
+ * A constructor function that creates a point in 2d
+ * @param {integer} x The x cooridinate of a 2D cartesian point 
+ * @param {integer} y The y cooridinate of a 2D cartesian point
+ */
+function Point(x,y){
+    this.x=x;
+    this.y=y;
 
+}
+
+var p1 = new Point(1,2);
+
+console.log(p1.x+","+p1.y);
