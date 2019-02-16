c29644035f947d76341601444cd7c6b298774845
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-16 16:43:29 -0500

    implemented the product method.

--- Project/JarvisMarchToJs/jarvisMarch.js
+++ Project/JarvisMarchToJs/jarvisMarch.js
@@ -6,23 +6,47 @@
 function Point(x,y) {
     this.x=x;
     this.y=y;
-
 }
 
+/**
+ * Creates a custom exception
+ * @param {String} errorMessage the custom error message passed by user 
+ */
 function customException(errorMessage) {
     this.errorMessage=errorMessage;
     this.name="custom exception";
 }
 
+/**
+ * Computes the orientation of 2 vectors via 3 points. Works in a couter-clockwise direction
+ * @param {Number} a the first 2D point
+ * @param {Number} b the second 2D point
+ * @param {Number} c the third 2D point
+ */
+function product(a,b,c) {
+    val = (b.x - a.x) * (c.x - b.x) -(b.x - a.x) * (c.y - b.y);
+
+    if (val == 0) {
+        return 0; // collinear
+    } else if(val>0){
+        return 1; // left of a
+    } else{
+        return 2;// right of a
+    }
+}
+
 function jarvisMarch(pointArray){
-/*     for (i = 0; i < pointArray.length;i++){
+    /*     
+    for (i = 0; i < pointArray.length;i++){
         console.log("Position "+i+" contains "+"("+pointArray[i].x+","+pointArray[i].y+")");
         
-    } */
+    } 
+    */
 
     if (pointArray.length < 3) {
         throw new customException('three or more points required');
     } 
+
 
 }
 
