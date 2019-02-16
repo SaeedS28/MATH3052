8550ae808053d66b0245e298608f298e1218b39e
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-16 14:39:20 -0500

    Created some sample points to compute the convex hull of.

--- Project/JarvisMarchToJs/jarvisMarch.js
+++ Project/JarvisMarchToJs/jarvisMarch.js
@@ -1,14 +1,26 @@
 /**
  * A constructor function that creates a point in 2d
- * @param {integer} x The x cooridinate of a 2D cartesian point 
- * @param {integer} y The y cooridinate of a 2D cartesian point
+ * @param {Number} x The x cooridinate of a 2D cartesian point 
+ * @param {Number} y The y cooridinate of a 2D cartesian point
  */
-function Point(x,y){
+function Point(x,y) {
     this.x=x;
     this.y=y;
 
 }
 
-var p1 = new Point(1,2);
 
-console.log(p1.x+","+p1.y);
+// Actual execution of the program.
+
+var points = [];
+
+points.push(new Point(0,3));
+points.push(new Point(2,3));
+points.push(new Point(1,1));
+points.push(new Point(2,1));
+points.push(new Point(3,0));
+points.push(new Point(0,0));
+points.push(new Point(3,3));
+points.push(new Point(2,6));
+
+console.log("Size of the array is: "+points.length);
